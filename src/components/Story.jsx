efa4bb870a1865758b7fb60f03425f9cf2b68e9a
Ghofrane Ben Hmaid <ghofranebh90@gmail.com>
2022-12-02 21:39:04 +0100

    :boom: add story list

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -1,7 +1,37 @@
 import React from "react";
-
+import "./story.scss";
 const Story = (props) => {
-  return <li>{props.title}</li>;
+
+  const time = new Date(+props.time * 1000).toLocaleString("dk-DK");
+  return (
+    <div className="cart">
+      <span className="cart__date"> {time} </span>
+      <h3 className="cart__title">
+        <a
+          className="cursor-pointer text-3xl"
+          href={props.url}
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          {props.title}
+        </a>
+      </h3>
+      <span className="cart__score">{props.score} points</span>
+      <span className="cart__author">
+        {" "}
+        by{" "}
+        <a
+          className="cursor-pointer text-3xl"
+          href={props.url}
+          target="_blank"
+          rel="noreferrer"
+        >
+          {props.by}
+        </a>{" "}
+      </span>
+    </div>
+  );
 };
 
 export default Story;
--- src/components/StoryList.jsx
+++ src/components/StoryList.jsx
@@ -2,10 +2,20 @@ import React from "react";
 import Story from "./Story";
 
 const StoryList = (props) => {
+  if (props.stories.length === 0) {
+    return <p className="center">No stories found!!</p>;
+  }
   return (
     <ul>
-      {props.stories.map((story) => (
-        <Story key={story.title} title={story.title} />
+      {props.stories?.map((story) => (
+        <Story
+          key={story.title}
+          title={story.title}
+          score={story.score}
+          url={story.url}
+          by={story.by}
+          time={story.time}
+        />
       ))}
     </ul>
   );
