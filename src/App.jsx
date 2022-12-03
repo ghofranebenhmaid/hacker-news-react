df4379232f06f4216ebff4381fa7621a81693a62
Ghofrane Ben Hmaid <ghofranebh90@gmail.com>
2022-12-03 17:11:13 +0100

    :bulb: Add image

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,42 @@
-import { useEffect, useState } from "react";
-import StoryList from "./components/StoryList";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Fragment, useCallback, useEffect, useState } from "react";
+import StoryList from "./components/stories/StoryList";
 
-import { fetchStories, fetchTopStoriesIds } from "./utils";
-
-const getRandom = (arr, count) => {
-  let _arr = [...arr];
-  return [...Array(count)].map(
-    () => _arr.splice(Math.floor(Math.random() * _arr.length), 1)[0]
-  );
-};
+import {
+  getRandom,
+  fetchAuthors,
+  fetchStories,
+  fetchTopStoriesIds,
+} from "./utils";
+import AuthorPage from "./pages/AuthorPage";
 
 function App() {
   const [stories, setStories] = useState([]);
+  const [name, setName] = useState([]);
   const [topStoriesIds, setTopStoriesIds] = useState([]);
   const [loading, setLoading] = useState(true);
 
-  const loadStories = async () => {
+  const loadStories = useCallback(async () => {
     setLoading(true);
     const topStoriesIdsData = await fetchTopStoriesIds();
     setTopStoriesIds(topStoriesIdsData);
     const storiesData = await fetchStories(topStoriesIdsData);
     const randomStory = getRandom(storiesData, 10);
+
     const sorted = randomStory.sort((a, b) => +a.score - +b.score);
-    console.log(sorted.by);
+
+    const authors = sorted.map((item) => item.by);
+    const by = await fetchAuthors(authors);
+
+    setName(by);
+    // console.log(name);
     setStories(sorted);
     setLoading(false);
-  };
+  }, []);
 
   useEffect(() => {
     loadStories();
-  }, []);
+  }, [loadStories]);
 
   return (
     <main className="App">
